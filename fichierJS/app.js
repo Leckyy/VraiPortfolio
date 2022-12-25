a9fbb64595a6893691e3cc4e1c24a92cba134ee8
tristan <90345150+Leckyy@users.noreply.github.com>
2022-12-25 16:07:44 +0100

    Add files via upload

--- fichierJS/app.js
+++ fichierJS/app.js
@@ -43,3 +43,114 @@ var diapo = 0;
     diapo="1";
   }
 
+
+
+
+  var fond =0;
+
+  document.getElementById('FondCouleur').addEventListener("click",function(){
+
+    if(fond === 0){
+      document.querySelector("body").classList.add("fondbody");
+      document.querySelector("nav").classList.add("FondMenu");
+      document.querySelector(".acceuilMenu").classList.add("FondMenu");
+    document.querySelector(".acceuilMenu").classList.add("traitBlanc");
+    document.querySelector(".langagesMenu").classList.add("FondMenu");
+    document.querySelector(".langagesMenu").classList.add("traitBlanc");
+    document.querySelector(".projetsMenu").classList.add("FondMenu");
+    document.querySelector(".projetsMenu").classList.add("traitBlanc");
+    document.querySelector(".formationsMenu").classList.add("FondMenu");
+    document.querySelector(".formationsMenu").classList.add("traitBlanc");
+    document.querySelector(".stagesMenu").classList.add("FondMenu");
+    document.querySelector(".stagesMenu").classList.add("traitBlanc");
+    document.querySelector(".veilleMenu").classList.add("FondMenu");
+    document.querySelector(".veilleMenu").classList.add("traitBlanc");
+    document.getElementById('FondCouleur').innerHTML="☀️";
+      document.getElementById('div1').classList.add("noir");
+      document.querySelector(".TitreLangage").classList.add("noir");
+      document.querySelector(".TexteLangage").classList.add("noir");
+      document.querySelector(".TitreProjet").classList.add("noir");
+      document.querySelector(".TexteProjet").classList.add("noir");
+      document.querySelector(".TitreFormation").classList.add("noir");
+      document.querySelector(".TexteFormation").classList.add("noir");
+      document.querySelector(".TitreStage").classList.add("noir");
+      document.querySelector(".TexteStage").classList.add("noir");
+      document.querySelector(".TitreVeille").classList.add("noir");
+      document.querySelector(".TexteVeille").classList.add("noir");
+      document.querySelector(".d1VeilleTechno").classList.add("noir");
+      document.querySelector(".d2VeilleTechno2").classList.add("noir");
+      document.querySelector(".d1IA").classList.add("noir");
+      localStorage.setItem("FondCouleur", "20");
+      fond++;
+    }
+
+    else{
+      document.querySelector("body").classList.remove("fondbody");
+      document.querySelector("nav").classList.remove("FondMenu");
+      document.querySelector(".acceuilMenu").classList.remove("FondMenu");
+    document.querySelector(".acceuilMenu").classList.remove("traitBlanc");
+    document.querySelector(".langagesMenu").classList.remove("FondMenu");
+    document.querySelector(".langagesMenu").classList.remove("traitBlanc");
+    document.querySelector(".projetsMenu").classList.remove("FondMenu");
+    document.querySelector(".projetsMenu").classList.remove("traitBlanc");
+    document.querySelector(".formationsMenu").classList.remove("FondMenu");
+    document.querySelector(".formationsMenu").classList.remove("traitBlanc");
+    document.querySelector(".stagesMenu").classList.remove("FondMenu");
+    document.querySelector(".stagesMenu").classList.remove("traitBlanc");
+    document.querySelector(".veilleMenu").classList.remove("FondMenu");
+    document.querySelector(".veilleMenu").classList.remove("traitBlanc");
+    document.getElementById('FondCouleur').innerHTML="🌙";
+      document.getElementById('div1').classList.remove("noir");
+      document.querySelector(".TitreLangage").classList.remove("noir");
+      document.querySelector(".TexteLangage").classList.remove("noir");
+      document.querySelector(".TitreProjet").classList.remove("noir");
+      document.querySelector(".TexteProjet").classList.remove("noir");
+      document.querySelector(".TitreFormation").classList.remove("noir");
+      document.querySelector(".TexteFormation").classList.remove("noir");
+      document.querySelector(".TitreStage").classList.remove("noir");
+      document.querySelector(".TexteStage").classList.remove("noir");
+      document.querySelector(".TitreVeille").classList.remove("noir");
+      document.querySelector(".TexteVeille").classList.remove("noir");
+      document.querySelector(".d1VeilleTechno").classList.remove("noir");
+      document.querySelector(".d2VeilleTechno2").classList.remove("noir");
+      document.querySelector(".d1IA").classList.remove("noir");
+      localStorage.removeItem("FondCouleur");
+      fond--;
+    }
+
+
+  })
+
+
+  if(localStorage.hasOwnProperty('FondCouleur')){
+    document.querySelector("body").classList.add("fondbody");
+    document.querySelector("nav").classList.add("FondMenu");
+    document.querySelector(".acceuilMenu").classList.add("FondMenu");
+    document.querySelector(".acceuilMenu").classList.add("traitBlanc");
+    document.querySelector(".langagesMenu").classList.add("FondMenu");
+    document.querySelector(".langagesMenu").classList.add("traitBlanc");
+    document.querySelector(".projetsMenu").classList.add("FondMenu");
+    document.querySelector(".projetsMenu").classList.add("traitBlanc");
+    document.querySelector(".formationsMenu").classList.add("FondMenu");
+    document.querySelector(".formationsMenu").classList.add("traitBlanc");
+    document.querySelector(".stagesMenu").classList.add("FondMenu");
+    document.querySelector(".stagesMenu").classList.add("traitBlanc");
+    document.querySelector(".veilleMenu").classList.add("FondMenu");
+    document.querySelector(".veilleMenu").classList.add("traitBlanc");
+    document.getElementById('FondCouleur').innerHTML="☀️";
+      document.getElementById('div1').classList.add("noir");
+      document.querySelector(".TitreLangage").classList.add("noir");
+      document.querySelector(".TexteLangage").classList.add("noir");
+      document.querySelector(".TitreProjet").classList.add("noir");
+      document.querySelector(".TexteProjet").classList.add("noir");
+      document.querySelector(".TitreFormation").classList.add("noir");
+      document.querySelector(".TexteFormation").classList.add("noir");
+      document.querySelector(".TitreStage").classList.add("noir");
+      document.querySelector(".TexteStage").classList.add("noir");
+      document.querySelector(".TitreVeille").classList.add("noir");
+      document.querySelector(".TexteVeille").classList.add("noir");
+      document.querySelector(".d1VeilleTechno").classList.add("noir");
+      document.querySelector(".d2VeilleTechno2").classList.add("noir");
+      document.querySelector(".d1IA").classList.add("noir");
+      fond="1";
+  }
